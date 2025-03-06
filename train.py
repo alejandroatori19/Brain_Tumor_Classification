52f0a41914233ef6bdd44ba62685e7aae9cd0207
alejandroatori19 <alejandroatori@gmail.com>
2025-03-06 21:10:38 +0100

    Updated EarlyStop Class
    Updated EarlyStop Class implementing methods to check if the conditions are met, a function that saves temporarily the model and other one to restore it

--- train.py
+++ train.py
@@ -34,18 +34,28 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 DECIMALS = 5
 
 # ----------------------------------
+# NEURAL NET CLASS
+# ----------------------------------
 
-# Class that manage the neural net
 class ResNet18Classifier(nn.Module):
-    def __init__(self, num_classes=4):
+    def __init__(self, dropout_prob=0.5, num_classes=4):
         super(ResNet18Classifier, self).__init__()
         self.model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)  # Load pre-trained ResNet18
-        in_features = self.model.fc.in_features  # Get the number of input features of the last layer
-        self.model.fc = nn.Linear(in_features, num_classes)  # Replace the fc layer
+        
+        # Modify the fully connected layer to include Dropout
+        num_features = self.model.fc.in_features
+        self.model.fc = nn.Sequential(
+            nn.Dropout(p=dropout_prob),  # Dropout before FC layer
+            nn.Linear(num_features, num_classes)
+        )
 
+    # -------------------
+    
     def forward(self, x):
         return self.model(x)
 
+# --------------------------------
+# DATASET CLASS
 # --------------------------------
 
 # Class that handles the dataset
@@ -54,7 +64,8 @@ class BrainTumorDataset (Dataset):
     transform = None
     data = None
 
-    # Requires dataset_path to work correctly. Transform is not need but it´s recomendable
+    # ------------------------------------------------
+    
     def __init__ (self, dataset_path, transform=None):
         # Initialize attributes        
         self.dataset_path = dataset_path
@@ -69,11 +80,13 @@ class BrainTumorDataset (Dataset):
             for file in os.listdir (label_path):
                 image_path = os.path.join (label_path, file)
                 self.data.append ((image_path, index_label))
-                    
-    # Return the length of the dataset
+    
+    # ----------------
+    
     def __len__(self):
         return len (self.data)
     
+    # -------------------------
     def __getitem__(self, idx):
         image_path, label = self.data[idx]
         image = Image.open (image_path).convert("RGB")
@@ -84,6 +97,8 @@ class BrainTumorDataset (Dataset):
         return image, label 
 
 # --------------
+# EARLY STOP
+# --------------
 
 class EarlyStop:
     patience = None
@@ -92,9 +107,9 @@ class EarlyStop:
     verbose = None
     best_loss = None
     counter = None
-    stop_training = None
     
-
+    # ------------------------------------------------------------------
+    
     def __init__(self, patience=2, threshold_loss=0.001, verbose=False):
         self.patience = patience
         self.threshold_loss = threshold_loss        
@@ -102,18 +117,71 @@ class EarlyStop:
         
         self.best_loss = np.inf
         self.counter = 0
-        self.stop_training = False
+    
+    # --------------------------------------------------
+        
+    def checking_conditions (self, epoch_result, model):
+        # Check if the loss is lower than the best loss.
+        if self.best_loss > epoch_result['Test Loss'] :
+            self.best_loss = epoch_result['Test Loss']
+            self.counter = 0
+            self.best_model = self.save_model_temporaly (model)
+            if self.verbose:
+                print (f"EARLY STOP (EPOCH - {epoch_result['Epoch']}): The model is learning correctly. (Best Loss: {self.best_loss}).")
+        else:
+            self.counter += 1
+            if self.verbose:
+                print (f"EARLY STOP (EPOCH - {epoch_result['Epoch']}): The model is not learning correctly (Counter: {self.counter}).")
 
-# -----------------------------------------------
+            # Check if the counter is greater or equal than the patience and end the program by returning True
+            if self.counter >= self.patience:
+                if self.verbose:
+                    print (f"EARLY STOP (EPOCH - {epoch_result['Epoch']}): The model has reached the patience of {self.patience} epochs without learning correctly. The program will end.")
+
+                return True
+            
+        # If the counter is less than the patience, return False
+        if epoch_result['Test Loss'] < self.threshold_loss:
+            if self.verbose:
+                print (f"EARLY STOP (EPOCH - {epoch_result['Epoch']}): The model has reached the threshold of {self.threshold_loss} loss. The program will end.")
+            return True
+    
+        return False
+    
+    # -------------------------------------
+    
+    def save_model_temporaly (self, model):
+        # Help to remove the temporal model and avoid overwriting
+        if os.path.exists (os.path.join (os.getcwd (), "models", "temp_model.pth")):
+            os.remove (os.path.join (os.getcwd (), "models", "temp_model.pth"))
+        
+        # Save the model
+        torch.save (model.state_dict (), os.path.join (os.getcwd (), "models", "temp_model.pth"))
+    
+    # ---------------------------
+    
+    def remove_temp_model (self):
+        if os.path.exists (os.path.join (os.getcwd (), "models", "temp_model.pth")):
+            os.remove (os.path.join (os.getcwd (), "models", "temp_model.pth"))
+    
+    # -----------------------
+      
+    def restore_model (self):
+        model = ResNet18Classifier ()
+        return model.load_state_dict(torch.load(os.path.join(os.getcwd(), "models", "temp_model.pth")))
+
+# ---------------------------------------------------
+# TRAIN PROCESS
+# ---------------------------------------------------
 
 def train_model (model, train_data, optimizer, loss):
     # Setting the model for training
     model.train ()
     
     # Initializing variables
-    train_loss = 32
-    train_correct = 32
-    train_size = 32
+    train_loss = 0
+    train_correct = 0
+    train_size = 0
     
     # Getting the time of the beginning of the training
     initTime = time.time()
@@ -149,15 +217,17 @@ def train_model (model, train_data, optimizer, loss):
     return train_loss, train_size, train_correct, end_time - initTime
     
 # -------------------------------------
+# TEST PROCESS
+# -------------------------------------
 
 def test_model(model, test_data, loss):
     # Setting the model for testing
     model.eval ()
     
     # Initializing variables
-    test_loss = 32
-    test_correct = 32
-    test_size = 32
+    test_loss = 0
+    test_correct = 0
+    test_size = 0
     
     # Getting the time of the beginning of the testing
     initTime = time.time()
@@ -167,7 +237,6 @@ def test_model(model, test_data, loss):
         
         # Processing the set of images
         for batch_idx, (images, labels) in enumerate (test_data):
-            
             # Converting data into tensors
             images_tensor = images.to (device=torch.device (DEVICE))
             labels_tensor = labels.to (device=torch.device (DEVICE))
@@ -193,7 +262,9 @@ def test_model(model, test_data, loss):
     
     return test_loss, test_size, test_correct, end_time - initTime
 
-# ------------------------------------------------------------------------------
+# -------------------------
+# SAVE RESULTS METHOD
+# -------------------------
 
 def save_results(results, epoch, train_loss, test_loss, train_data_size, test_data_size, train_correct_predictions, test_correct_predictions, train_time, test_time):
     # Getting the final values
@@ -206,8 +277,8 @@ def save_results(results, epoch, train_loss, test_loss, train_data_size, test_da
     train_correct_predictions_percentage = train_correct_predictions / train_data_size * 100
     train_incorrect_predictions_percentage = 100 - train_correct_predictions_percentage
         
-    train_time_formatted = str (datetime.timedelta (seconds=train_time))
-    test_time_formatted = str (datetime.timedelta (seconds=test_time))
+    train_time_formatted = train_time
+    test_time_formatted = test_time
         
     # Add new values to the results
     results.loc[len(results)] = [int (epoch + 1), 
@@ -217,6 +288,9 @@ def save_results(results, epoch, train_loss, test_loss, train_data_size, test_da
                                  round (test_correct_predictions_percentage, DECIMALS), round (test_incorrect_predictions_percentage, DECIMALS),
                                  train_time_formatted, test_time_formatted]
     
+# ---------------------------------------------
+# SAVE RESULTS INTO DISK
+# ---------------------------------------------
 def save_results_into_disk (results_dataframe):
     results_dataframe["Epoch"] = results_dataframe["Epoch"].astype(int)
 
@@ -224,30 +298,35 @@ def save_results_into_disk (results_dataframe):
     results_dataframe.to_csv("results/results.csv", index=False)
 
 # --------------------------
+# PRINT RESULTS METHOD
+# --------------------------
 
 def print_results (results):
     results_epoch = results.iloc[-1]
-    
+
     print (f"------ STARTING EPOCH {results_epoch['Epoch']} ------")
-    print (f"\tTrain Loss: {round (results_epoch['Train Loss'], DECIMALS)}")
-    print (f"\tTrain Accuracy: {round (results_epoch['Train Accuracy'], DECIMALS)}")
-    print (f"\tTrain Time: {round (results_epoch['Time (Train)'], DECIMALS)} seconds")
-    print (f"\tTest Loss: {round (results_epoch['Test Loss'], DECIMALS)}")
-    print (f"\tTest Accuracy: {round (results_epoch['Test Accuracy'], DECIMALS)}")
-    print (f"\tTest Time: {round (results_epoch['Time (Test)'], DECIMALS)} seconds")
+    print (f"\tTrain Loss: {round (results_epoch['Train Loss'], DECIMALS)}.")
+    print (f"\tTrain Accuracy: {round (results_epoch['Train Accuracy'], DECIMALS)}.")
+    print (f"\tTrain Time: {int (results_epoch['Time (Train)'] / 60)} min & {int (results_epoch['Time (Train)'] % 60)} sec.")
+    print (f"\tTest Loss: {round (results_epoch['Test Loss'], DECIMALS)}.")
+    print (f"\tTest Accuracy: {round (results_epoch['Test Accuracy'], DECIMALS)}.")
+    print (f"\tTest Time: {int (results_epoch['Time (Test)'] / 60)} min & {int (results_epoch['Time (Test)'] % 60)} sec.")
     print (f"------ ENDING EPOCH {results_epoch['Epoch']} ------")
     print ("\n\n")
 
 # --------------- 
+# MAIN CODE
+# ---------------
 
 def main_code ():
     # Variables
     path = r"C:\Users\Usuario\Desktop\proyectos\Brain_Tumor_Classification\data"    # datset path
     
     # Preparate the model
-    neuralNet = ResNet18Classifier (len (labels))
+    neuralNet = ResNet18Classifier (dropout_prob=0.5, num_classes=len (labels))
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(neuralNet.parameters(), lr=learning_rate)
+    earlyStop = EarlyStop (patience=2, threshold_loss=0.001, verbose=True)
     
     # Create dataset instances with transforms
     train_dataset = BrainTumorDataset(os.path.join(path, 'Training'), transform=transform)
@@ -280,10 +359,16 @@ def main_code ():
         
         print_results (results)
         
+        if earlyStop.checking_conditions (results.iloc[-1], neuralNet):
+            break
 
     # Saving data
     save_results_into_disk (results)
     save(neuralNet.state_dict(), os.path.join(os.getcwd (), "models", "model.pth"))
+
+# ------------------------
+# EXECUTE MAIN CODE
+# ------------------------
 
 if __name__ == '__main__':
     main_code ()
